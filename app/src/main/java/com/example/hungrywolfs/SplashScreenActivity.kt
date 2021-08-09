dd50506ddc19330f9d1bd27ea3572dd4f7c2d617
Grad Vasile <vasile.grad2@gmail.com>
2021-08-09 22:30:43 +0300

    Create SplashScreen Activity

---
+++ app/src/main/java/com/example/hungrywolfs/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hungrywolfs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 1500
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME)
+    }
+}
