a559abbe176033d41bd3d34b7ddd6e26c3f5bba2
Grad Vasile <vasile.grad2@gmail.com>
2021-08-10 14:34:18 +0300

    Change the icons. Add the fragments

--- app/src/main/java/com/example/hungrywolfs/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolfs/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.hungrywolfs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hungrywolfs.fragments.FavoritesFragment
+import com.example.hungrywolfs.fragments.HomeFragment
+import com.example.hungrywolfs.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val favoritesFragment = FavoritesFragment()
+        val settingsFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_wrapper, fragment)
+            commit()
+        }
 }
