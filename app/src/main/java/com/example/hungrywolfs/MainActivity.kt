d267e0cc5f2fc6d8abae3bcdf575748545dafb4a
Grad Vasile <vasile.grad2@gmail.com>
2021-08-11 11:45:43 +0300

    Add other changes to the bottom navigation

--- app/src/main/java/com/example/hungrywolfs/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolfs/MainActivity.kt
@@ -2,26 +2,26 @@ package com.example.hungrywolfs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.example.hungrywolfs.fragments.FavoritesFragment
-import com.example.hungrywolfs.fragments.HomeFragment
-import com.example.hungrywolfs.fragments.ProfileFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        val homeFragment = HomeFragment()
-        val favoritesFragment = FavoritesFragment()
-        val settingsFragment = ProfileFragment()
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        makeCurrentFragment(homeFragment)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        bottomNavigation.setupWithNavController(navController)
     }
-
-    private fun makeCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frame_wrapper, fragment)
-            commit()
-        }
 }
