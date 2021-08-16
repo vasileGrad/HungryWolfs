84680a9eeba4da166ef6aa4579c2cc230cd0457d
Grad Vasile <vasile.grad2@gmail.com>
2021-08-16 11:09:19 +0300

    Reposition the Meal RecyclerView to the initial position. Create the divider between the meal elements

--- app/src/main/java/com/example/hungrywolfs/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/hungrywolfs/fragments/HomeFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hungrywolfs.R
 import com.example.hungrywolfs.adapters.CategoryAdapter
 import com.example.hungrywolfs.adapters.MealAdapter
 import com.example.hungrywolfs.databinding.FragmentHomeBinding
@@ -32,11 +36,19 @@ class HomeFragment : Fragment() {
         val categoryAdapter = CategoryAdapter()
         val mealAdapter = MealAdapter()
 
+        val divider = DividerItemDecoration(requireContext(), RecyclerView.HORIZONTAL)
+        ContextCompat.getDrawable(requireContext(), R.drawable.divider)
+            ?.let { divider.setDrawable(it) }
+        binding.recyclerViewMeals.addItemDecoration(divider)
+
         binding.recyclerViewCategories.adapter = categoryAdapter
         binding.recyclerViewMeals.adapter = mealAdapter
 
         viewModel.info.observe(viewLifecycleOwner) { categoryAdapter.setCategories(it.categories) }
-        viewModel.mealInfo.observe(viewLifecycleOwner) { mealAdapter.setMeals(it.meals) }
+        viewModel.mealInfo.observe(viewLifecycleOwner) {
+            mealAdapter.setMeals(it.meals)
+            binding.recyclerViewMeals.scrollToPosition(0)
+        }
         categoryAdapter.selected.observe(viewLifecycleOwner) { viewModel.getSelectedMeals(it) }
     }
 }
